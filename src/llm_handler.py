2afb705b9dc4be950cc15de53bc60b68683d4fff
rakesh <rakesh_anvekar@gmail.com>
2025-11-09 18:27:41 +0530

    code push

--- src/llm_handler.py
+++ src/llm_handler.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class LLMHandler:
     def __init__(self, model="llama-3.1-8b-instant", temperature=0.1, max_tokens=1024):
-        api_key = os.getenv("GROQ_API_KEY")       
+        api_key = os.getenv("GROQ_API_KEY")        
         self.llm = ChatGroq(api_key=api_key, model=model, temperature=temperature, max_tokens=max_tokens)
 
     def generate_answer(self, context, query):
